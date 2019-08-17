17bb1da6c9b0c12adccbaec757c23e6ee4d6b807
August Ye <augustye@gmail.com>
2019-08-17 10:47:12 +0800

    decompose 3rd image

--- random_agent.py
+++ random_agent.py
@@ -9,28 +9,38 @@ env.seed(0)
 obs   = 0
 count = 0
 done  = True
-imgs  = []
-for i in range(93):
+hist  = []
+for i in range(94):
 	if done:
 		env.reset()
 	random_action = env.action_space.sample()
 	new_obs, rew, done, info = env.step(random_action)
-	diff = new_obs-obs
-	if np.any(diff):
+	if np.any(new_obs-obs):
 		count += 1
 		img = Image.fromarray(new_obs, 'RGB')
 		print("i:%3d, count:%d"%(i,count))
-		if len(imgs) == 0:
+		
+		if len(hist) == 0:
 			img.save('components/background.png')
-		else:
-			mask = Image.fromarray(np.minimum(diff,1)*255, 'RGB').convert('1')
+
+		if len(hist) == 1:
+			mask = Image.fromarray(np.minimum(new_obs-hist[0],1)*255, 'RGB').convert('1')
 			mask.save('components/mask.png')
 
 			sprite = Image.new("RGBA", img.size)
 			sprite.paste(img, mask)
 			sprite.save('components/sprite.png')
 
-		imgs.append(img)
+		if len(hist) == 2:
+			mask = Image.fromarray(np.minimum(new_obs-hist[0],1)*255, 'RGB').convert('1')
+			mask.save('components/mask2.png')
+
+			sprite = Image.new("RGBA", img.size)
+			sprite.paste(img, mask)
+			sprite.save('components/sprite2.png')
+
+
+		hist.append(new_obs)
 		img.save(f'images/{count}.png')
 	obs = new_obs
 	env.render()
