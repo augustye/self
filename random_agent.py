fae15cb4401feec40ce6907f37dbcdd018b067c5
August Ye <augustye@gmail.com>
2019-08-18 10:12:56 +0800

    save images

--- random_agent.py
+++ random_agent.py
@@ -9,8 +9,7 @@ env.seed(0)
 obs   = 0
 count = 0
 done  = True
-hist  = []
-for i in range(94):
+for i in range(195):
 	if done:
 		env.reset()
 	random_action = env.action_space.sample()
@@ -19,28 +18,6 @@ for i in range(94):
 		count += 1
 		img = Image.fromarray(new_obs, 'RGB')
 		print("i:%3d, count:%d"%(i,count))
-		
-		if len(hist) == 0:
-			img.save('components/background.png')
-
-		if len(hist) == 1:
-			mask = Image.fromarray(np.minimum(new_obs-hist[0],1)*255, 'RGB').convert('1')
-			mask.save('components/mask.png')
-
-			sprite = Image.new("RGBA", img.size)
-			sprite.paste(img, mask)
-			sprite.save('components/sprite.png')
-
-		if len(hist) == 2:
-			mask = Image.fromarray(np.minimum(new_obs-hist[0],1)*255, 'RGB').convert('1')
-			mask.save('components/mask2.png')
-
-			sprite = Image.new("RGBA", img.size)
-			sprite.paste(img, mask)
-			sprite.save('components/sprite2.png')
-
-
-		hist.append(new_obs)
 		img.save(f'images/{count}.png')
 	obs = new_obs
 	env.render()
