d33983801f5c66ead1944f57c9d9ca65f22e3b78
August Ye <augustye@gmail.com>
2019-08-18 16:10:04 +0800

    simplify code

--- self.py
+++ self.py
@@ -2,8 +2,8 @@ import gym
 import numpy as np
 import os
 from PIL import Image
-from IPython import display
 
+IMAGES = 4
 WIDTH  = 210
 HEIGHT = 160
 hist  = []
@@ -11,19 +11,20 @@ hist  = []
 def generate_images():
 	env = gym.make("KungFuMaster-v0")
 	env.seed(0)
-	obs   = 0
-	count = 0
-	done  = True
-	for i in range(94):
+	obs,done = 0,True
+	frames,images = 0,0
+	while images < IMAGES:
 		if done:
 			env.reset()
 		random_action = env.action_space.sample()
-		new_obs, rew, done, info = env.step(random_action)
+		new_obs,_,done,_= env.step(random_action)
 		if np.any(new_obs-obs):
-			count += 1
+			f,i = frames,images
+			print("frame %05d, image %05d"%(f,i))
 			img = Image.fromarray(new_obs, 'RGB')
-			print("i:%3d, count:%d"%(i,count))
-			img.save('images_%05d.png'%(count))
+			img.save('images_%05d.png'%(images))
+			images += 1
+		frames += 1
 		obs = new_obs
 	env.close()
 
@@ -33,49 +34,53 @@ def process_images():
 	for file in files:
 		if file.startswith('images_'):
 				img = Image.open(file)
-				process_image(img)
+				obs = np.array(img)
+				index = len(hist)
+				hist.append(obs)	
+				process_image(index, img)
 
-def get_background(count, new_obs, img):
+def save_background(index, img):
 	img.save('components_background.png')
 
-def get_sprite(count, new_obs, img):
-	mask = Image.fromarray(np.minimum(new_obs-hist[0],1)*255, 'RGB').convert('1')
-	mask.save('components_mask%d.png'%(count))
+def save_sprite(index, img):
+	diff = hist[index] - hist[0]
+	diff = np.minimum(diff, 1)*255
+	mask = Image.fromarray(diff, 'RGB')
+	mask = mask.convert('1')
+	mask.save('components_mask%d.png'%(index))
 
 	sprite = Image.new("RGBA", img.size)
 	sprite.paste(img, mask)
-	sprite.save('components_sprite%d.png'%(count))
+	sprite.save('components_sprite%d.png'%(index))
 
-
-def process_image(img):
-	new_obs = np.array(img)
-	count = len(hist)
-	hist.append(new_obs)	
+def process_image(index, img):
 			
-	if count == 0:
-		get_background(count, new_obs, img)
+	if index == 0:
+		save_background(index, img)
 
-	if count == 1:
-		get_sprite(count, new_obs, img)
+	if index == 1:
+		save_sprite(index, img)
 
-	if count == 2:
-		get_sprite(count, new_obs, img)
+	if index == 2:
+		save_sprite(index, img)
 					
 def show_images():
+	from IPython import display as d
 	files = os.listdir(".")
 	files.sort()
 	for file in files:
-	  if file.endswith(".png"):
-	    with open(file,'rb') as f:
-	      print(file)
-	      display.display(display.Image(data=f.read()))
-
-#sprite数据结构: 多个可能的造型，当前位置，当前造型，造型重叠判断
-generate_images()
-process_images()
-show_images()
+		if file.endswith(".png"):
+			with open(file,'rb') as f:
+				print(file)
+				d.display(d.Image(data=f.read()))
+				
+if __name__ == '__main__':
+	generate_images()
+	process_images()
+	show_images()
 	
-
+#sprite数据结构: 造型列表，x/y，遮挡标志，当前造型id
+#sprite造型重叠判断
 
 		
 
