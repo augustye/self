c75d02aa21be0870b508f1b07fe4784accf6aff1
August Ye <augustye@gmail.com>
2019-08-21 12:55:00 +0800

    change tabs to spaces

--- self.py
+++ self.py
@@ -10,106 +10,105 @@ sprites = []
 worlds  = []
 
 class Pose:
-	def __init__(self, img, masks=[0,0,0,0]):
-		self.img = img
-		self.top_masked    = bool(masks[0])
-		self.right_masked  = bool(masks[1])
-		self.bottom_masked = bool(masks[2])
-		self.left_masked   = bool(masks[3])
+  def __init__(self, img, masks=[0,0,0,0]):
+    self.img = img
+    self.top_masked    = bool(masks[0])
+    self.right_masked  = bool(masks[1])
+    self.bottom_masked = bool(masks[2])
+    self.left_masked   = bool(masks[3])
 
 class Sprite:
-	def __init__(self, poses=[]):
-		self.poses = poses
+  def __init__(self, poses=[]):
+    self.poses = poses
 
 def process_images():
-	files = os.listdir('images')
-	files.sort()
-	for file in files:
-		img = Image.open('images/'+file)
-		obs = np.array(img)
-		index = len(hist)
-		hist.append(obs)	
-		if index < 3:
-			display(img)
-			get_world(index, img)	
+  files = os.listdir('images')
+  files.sort()
+  for file in files:
+    img = Image.open('images/'+file)
+    obs = np.array(img)
+    index = len(hist)
+    hist.append(obs)  
+    if index < 3:
+      display(img)
+      get_world(index, img)  
 
 def get_world(index, img):
-	
-	if index > 0:
-		diff = hist[index] - hist[0]
-		diff = np.minimum(diff, 1)*255
-		mask = Image.fromarray(diff, 'RGB')
-		mask = mask.convert('1')
-		img2 = Image.new("RGBA", img.size)
-		img2.paste(img, mask)
-		img  = img2
+  
+  if index > 0:
+    diff = hist[index] - hist[0]
+    diff = np.minimum(diff, 1)*255
+    mask = Image.fromarray(diff, 'RGB')
+    mask = mask.convert('1')
+    img2 = Image.new("RGBA", img.size)
+    img2.paste(img, mask)
+    img  = img2
 
-	bbox = img.getbbox()
-	im = img.crop(bbox)
-	print('bbox:',bbox)
-	
-	x,y = bbox[:2]
-	print('position',x,y)
-	
-	l_masked = (bbox[0] == 0)
-	t_masked = (bbox[1] == 0)
-	r_masked = (bbox[2] == WIDTH)
-	b_maksed = (bbox[3] == HEIGHT)
-	masks  = [t_masked,r_masked,b_maksed,l_masked]
-	pose   = Pose(im, masks)
-		
-	if index == 0:
-		sprite = Sprite([pose])
-		sprites.append(sprite)
-		sprite_id = 0
-		pose_id   = 0
-		world = [[sprite_id, pose_id, x, y]]
-	
-	if index == 1:
-		sprite = Sprite([pose])
-		sprites.append(sprite)
-		sprite_id = 1
-		pose_id   = 0
-		world = [worlds[0][0]]
-		world.append([sprite_id, pose_id, x, y])
+  bbox = img.getbbox()
+  im = img.crop(bbox)
+  print('bbox:',bbox)
+  
+  x,y = bbox[:2]
+  print('position',x,y)
+  
+  l_masked = (bbox[0] == 0)
+  t_masked = (bbox[1] == 0)
+  r_masked = (bbox[2] == WIDTH)
+  b_maksed = (bbox[3] == HEIGHT)
+  masks  = [t_masked,r_masked,b_maksed,l_masked]
+  pose   = Pose(im, masks)
+    
+  if index == 0:
+    sprite = Sprite([pose])
+    sprites.append(sprite)
+    sprite_id = 0
+    pose_id   = 0
+    world = [[sprite_id, pose_id, x, y]]
 
-	if index == 2:
-		sprites[1].poses.append(pose)
-		sprite_id = 1
-		pose_id   = 1
-		world = [worlds[0][0]]
-		world.append([sprite_id, pose_id, x, y])
-	
-	worlds.append(world)
-	print('world:', world)
+  if index == 1:
+    sprite = Sprite([pose])
+    sprites.append(sprite)
+    sprite_id = 1
+    pose_id   = 0
+    world = [worlds[0][0]]
+    world.append([sprite_id, pose_id, x, y])
+
+  if index == 2:
+    sprites[1].poses.append(pose)
+    sprite_id = 1
+    pose_id   = 1
+    world = [worlds[0][0]]
+    world.append([sprite_id, pose_id, x, y])
+  
+  worlds.append(world)
+  print('world:', world)
 
 def show_sprites():
-	print('sprites:')
-	for i in range(len(sprites)):
-		sprite = sprites[i]
-		imgs = []
-		height = 0
-		width  = 0
-		for pose in sprite.poses:
-			w,h = pose.img.size
-			height = max(height, h)
-			width += w+10
-		canvas = Image.new("RGBA", (width,height))
-		x = 0		
-		for pose in sprite.poses:
-			w,h = pose.img.size
-			canvas.paste(pose.img, (x,0,x+w,h))		
-			x += w+10
-		file = 'sprite_%03d.png'%(i)
-		print('saved:', file)
-		canvas.save(file)
-		display(canvas)
-																				
+  print('sprites:')
+  for i in range(len(sprites)):
+    sprite = sprites[i]
+    imgs = []
+    height = 0
+    width  = 0
+    for pose in sprite.poses:
+      w,h = pose.img.size
+      height = max(height, h)
+      width += w+10
+    canvas = Image.new("RGBA", (width,height))
+    x = 0    
+    for pose in sprite.poses:
+      w,h = pose.img.size
+      canvas.paste(pose.img, (x,0,x+w,h))    
+      x += w+10
+    file = 'sprite_%03d.png'%(i)
+    print('saved:', file)
+    canvas.save(file)
+    display(canvas)
+                                        
 if __name__ == '__main__':
-	print('version 2')
-	process_images()
-	show_sprites()
+  print('version 2')
+  process_images()
+  show_sprites()
 
 #为pose寻找已有的sprite id和pose id 
-
 
